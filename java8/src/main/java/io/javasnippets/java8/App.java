b5aac0ceb3e9b195444325df340aaa7d809b3bb8
Umashankar <Umashankar@192.168.0.3>
2016-10-13 09:08:22 +0530

    Java 8 Maven Bootstrap Project created!

---
+++ java8/src/main/java/io/javasnippets/java8/App.java
@@ -0,0 +1,14 @@
+package io.javasnippets.java8;
+
+/**
+ * 
+ * @author Umashankar
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
